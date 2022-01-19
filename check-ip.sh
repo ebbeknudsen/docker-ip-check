2e17830bac34a0b775eb46ffdc37d7b2b2e2f711
Ebbe Knudsen <edk0088@gmail.com>
2022-01-19 18:17:53 +0000

    Show (empty) if old IP not found

--- check-ip.sh
+++ check-ip.sh
@@ -6,25 +6,30 @@ ADDRESS_LOGFILE=/current-address
 touch $ADDRESS_LOGFILE
 
 OLD_IP=$(cat $ADDRESS_LOGFILE)
-CUR_IP=$(curl -s ipconfig.io)
-UNCHANGED_LOG="IP unchanged: $CUR_IP"
-CHANGED_LOG="Detected an IP change. Old: $OLD_IP, new: $CUR_IP"
+CURRENT_IP=$(curl -s ipconfig.io)
 
-if [ "$OLD_IP" == "$CUR_IP" ]; then
+if [ -z "$OLD_IP"]; then
+  echo "Old IP not found, setting to empty"
+  OLD_IP="(empty)"
+fi
+
+if [ "$OLD_IP" == "$CURRENT_IP" ]; then
+  UNCHANGED_LOG="IP unchanged: $CURRENT_IP"
   echo "$UNCHANGED_LOG"
   echo ""
 
 else
+  CHANGED_LOG="Detected an IP change. Old: $OLD_IP, new: $CURRENT_IP"
   echo "$CHANGED_LOG"
   echo ""
 
   echo "$(date): $CHANGED_LOG" >> $CHANGE_LOGFILE
 
-  echo "$CUR_IP" > $ADDRESS_LOGFILE
+  echo "$CURRENT_IP" > $ADDRESS_LOGFILE
 
-  /bin/bash /scripts/ip-changed.sh $CUR_IP $OLD_IP
+  /bin/bash /scripts/ip-changed.sh $CURRENT_IP $OLD_IP
 fi
 
-/bin/bash /scripts/ip-check-finished.sh $CUR_IP $OLD_IP
+/bin/bash /scripts/ip-check-finished.sh $CURRENT_IP $OLD_IP
 
 
